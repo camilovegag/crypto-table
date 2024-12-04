383778dd1d129a082d6567e859a4a6fbc2f796b3
camilovegag <camilovegag@unisabana.edu.co>
2024-12-03 22:58:04 -0500

    Add list component

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Trash } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
 
 interface Todo {
   id: number;
@@ -12,6 +14,7 @@ interface Todo {
 function List() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>("");
+  const { toast } = useToast(); // Toast hook
 
   useEffect(() => {
     const storedTodos = localStorage.getItem("todos");
@@ -20,12 +23,6 @@ function List() {
     }
   }, []);
 
-  //   useEffect(() => {
-  //     if (todos.length > 0) {
-  //       localStorage.setItem("todos", JSON.stringify(todos));
-  //     }
-  //   }, [todos]);
-
   const addTodo = () => {
     if (newTodo.trim() === "") return;
     const newTodoItem: Todo = {
@@ -33,17 +30,34 @@ function List() {
       text: newTodo,
       timestamp: Date.now(),
     };
-    setTodos([...todos, newTodoItem]);
-    localStorage.setItem("todos", JSON.stringify([...todos, newTodoItem]));
+    const updatedTodos = [newTodoItem, ...todos];
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
     setNewTodo("");
+    toast({
+      className: cn(
+        "top-0 right-0 flex fixed md:max-w-[220px] md:top-4 md:right-4"
+      ),
+      title: "Transacción agregada!",
+    });
   };
 
   const removeTodo = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-    localStorage.setItem(
-      "todos",
-      JSON.stringify(todos.filter((todo) => todo.id !== id))
-    );
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    toast({
+      className: cn(
+        "top-0 right-0 flex fixed md:max-w-[220px] md:top-4 md:right-4"
+      ),
+      title: "Transacción eliminada!",
+    });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
   };
 
   return (
@@ -54,25 +68,25 @@ function List() {
           type="text"
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="Agrega tu transacción acá"
         />
-        <Button onClick={addTodo}>
-          Agregar
-        </Button>
+        <Button onClick={addTodo}>Agregar</Button>
       </div>
-      <ul className="mt-4">
+      <ul className="flex flex-col gap-6 mt-4 overflow-auto h-96 border p-4 rounded-sm">
         {todos.map((todo) => (
           <li className="flex gap-4 items-start" key={todo.id}>
             <Button
+            className="hover:bg-red-300"
               variant="outline"
               size="icon"
               onClick={() => removeTodo(todo.id)}
             >
               <Trash />
             </Button>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
               <span>{todo.text}</span>
-              <span className="text-sm text-gray-500">
+              <span className="text-xs text-gray-500">
                 {new Date(todo.timestamp).toLocaleString()}
               </span>
             </div>
