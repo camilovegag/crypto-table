5577e350df709626c56be6c549214fdb1b3b4747
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2024-12-04 16:14:35 -0500

    Deployment (#4)
    * Install shadcn
    
    * Create app skeleton with dark theme toggler
    
    * Create full app
    
    * Add list component
    
    * Enhancements
    
    * Added tabs an lots of enhancements
    
    * Formating changes
    
    * Fix error
    
    * Changes to tabs
    
    * Delete tabs component

--- src/components/tab-content.tsx
+++ src/components/tab-content.tsx
@@ -1,39 +1,26 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import { CryptoTable } from "@/components/crypto-table";
-import { Plus } from "lucide-react";
 
-interface CryptoTableTabsContentProps {
+interface CryptoTableTabProps {
+  id: string;
+  timestamp: string;
   ethValue: string;
   ethBehavior: "up" | "down";
 }
 
-const CryptoTableTabsContent: React.FC<CryptoTableTabsContentProps> = ({
+export default function CryptoTableTab({
   ethValue,
   ethBehavior,
-}) => {
-  const [tableCount, setTableCount] = useState(1);
-
-  const addTable = () => {
-    setTableCount(tableCount + 1);
-  };
-
+  id,
+  timestamp,
+}: CryptoTableTabProps) {
   return (
-    <div className="flex flex-col gap-4">
-      <Button className="self-start mt-6" onClick={addTable}>
-        <Plus />
-        Añadir Tabla
-      </Button>
-      {Array.from({ length: tableCount }).map((_, index) => (
-        <CryptoTable
-          key={index}
-          index={index}
-          ethValue={ethValue}
-          ethBehavior={ethBehavior}
-        />
-      ))}
+    <div
+      className={`flex flex-col gap-4 w-full border-2 p-4 rounded-md ${
+        ethBehavior === "up" ? "border-green-200" : "border-red-200"
+      }`}
+    >
+      <p className="text-sm text-gray-500">Tabla creada {timestamp}</p>
+      <CryptoTable index={id} ethValue={ethValue} ethBehavior={ethBehavior} />
     </div>
   );
-};
-
-export default CryptoTableTabsContent;
+}
